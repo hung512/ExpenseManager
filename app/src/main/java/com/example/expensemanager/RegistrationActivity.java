1396b10fb0e39613354a3d10bcb78eedcbd0f338
hung512 <55794001+hung512@users.noreply.github.com>
2020-12-21 15:54:44 +0700

    Đăng nhập + Đăng kí

---
+++ app/src/main/java/com/example/expensemanager/RegistrationActivity.java
@@ -0,0 +1,17 @@
+package com.example.expensemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class RegistrationActivity extends AppCompatActivity {
+    private EditText mEmail;
+    private  EditText mPassword;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
