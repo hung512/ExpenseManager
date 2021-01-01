1a016c8b27b7271df0fad09199bf1fcea2602160
hung512 <55794001+hung512@users.noreply.github.com>
2021-01-01 10:57:20 +0700

    DateTimePicker

--- app/src/main/java/com/example/expensemanager/CalendarActivity.java
+++ app/src/main/java/com/example/expensemanager/CalendarActivity.java
@@ -1,43 +1,94 @@
 package com.example.expensemanager;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 
-import java.text.DateFormat;
 import java.util.Calendar;
 
-public class CalendarActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+public class CalendarActivity extends AppCompatActivity{
+
+    Button dateButton, timeButton;
+    TextView dateTextView, timeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        Button button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        dateButton = findViewById(R.id.dateButton);
+        timeButton = findViewById(R.id.timeButton);
+        dateTextView = findViewById(R.id.dateTextView);
+        timeTextView = findViewById(R.id.timeTextView);
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DialogFragment datePicker = new DatePickerFragment();
-                datePicker.show(getSupportFragmentManager(), "date picker");
+                handleDateButton();
+            }
+        });
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleTimeButton();
             }
         });
     }
 
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        Calendar c= Calendar.getInstance();
-        c.set(Calendar.YEAR, year);
-        c.set(Calendar.MONTH, month);
-        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
-        TextView textView = (TextView)findViewById(R.id.textView);
-        textView.setText(currentDateString);
+    private void handleTimeButton() {
+        Calendar calendar= Calendar.getInstance();
+        int HOUR= calendar.get(Calendar.HOUR);
+        int MINUTE= calendar.get(Calendar.MINUTE);
+
+        boolean is24HourFormat= DateFormat.is24HourFormat(this);
+
+        TimePickerDialog timePickerDialog= new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                String timeString= " " + hour + ":" + minute;
+                timeTextView.setText(timeString);
+
+//                Calendar calendar= Calendar.getInstance();
+//                calendar.set(Calendar.HOUR, hour);
+//                calendar.set(Calendar.MINUTE, minute);
+//
+//                CharSequence charSequence = DateFormat.format("hh:mm a", calendar);
+//                timeTextView.setText(charSequence);
+            }
+        }, HOUR, MINUTE, is24HourFormat);
+        timePickerDialog.show();
+    }
+
+    private void handleDateButton() {
+
+        Calendar cal= Calendar.getInstance();
+        int YEAR = cal.get(Calendar.YEAR);
+        int MONTH= cal.get(Calendar.MONTH);
+        int DATE= cal.get(Calendar.DATE);
+
+        DatePickerDialog datePickerDialog= new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+                String dateString = year + " " + month + " "+ date;
+                dateTextView.setText(dateString);
+
+                Calendar calendar= Calendar.getInstance();
+                int YEAR = calendar.get(Calendar.YEAR);
+                int MONTH= calendar.get(Calendar.MONTH);
+                int DATE= calendar.get(Calendar.DATE);
+
+                CharSequence dateCharSequence= DateFormat.format("EEE, dd MMM yyyy", calendar);
+                dateTextView.setText(dateCharSequence);
+            }
+        }, YEAR, MONTH, DATE);
+        datePickerDialog.show();
     }
 }
--- app/src/main/java/com/example/expensemanager/DatePickerFragment.java
+++ app/src/main/java/com/example/expensemanager/DatePickerFragment.java
@@ -2,7 +2,9 @@ package com.example.expensemanager;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.DatePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -10,15 +12,36 @@ import androidx.fragment.app.DialogFragment;
 
 import java.util.Calendar;
 
-public class DatePickerFragment extends DialogFragment {
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    public interface DatePickerListener{
+        void onDateSet(DatePicker datePicker, int year, int month, int day);
+    }
+    DatePickerListener mListener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try{
+            mListener=(DatePickerListener) context;
+        }catch (Exception e){
+            throw new ClassCastException(getActivity().toString()+ "Must Implements DatePickerListener");
+        }
+    }
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Calendar cal=Calendar.getInstance();
+        int year= cal.get(Calendar.YEAR);
+        int month= cal.get(Calendar.MONTH);
+        int day= cal.get(Calendar.DAY_OF_MONTH);
 
-        Calendar c= Calendar.getInstance();
-        int year= c.get(Calendar.YEAR);
-        int month= c.get(Calendar.MONTH);
-        int day= c.get(Calendar.DAY_OF_MONTH);
-        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        mListener.onDateSet(datePicker, i, i1, i2);
     }
 }
